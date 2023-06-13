9985c129c9cfb067989a5a6a7427ddc234fc92c1
dragontaek-lee <dragontaek98@naver.com>
2023-06-14 03:12:38 +0900

    feat: init project #1

---
+++ src/lambda.ts
@@ -0,0 +1,14 @@
+"use strict";
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { SNSClient } from "@aws-sdk/client-sns";
+
+const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+const snsClient = new SNSClient({ region: process.env.AWS_REGION });
+
+module.exports.lambda = async (event: any, context: any, callback: any) => {
+  callback(null, {
+    statusCode: 200,
+    body: "hello push server!",
+  });
+};
