75b3f6ef489d869b2f07a88d07705113c61d6c3d
dudgns3tp <c_yh0327@naver.com>
2023-06-17 01:54:18 +0900

    chore: lint, prettier setting

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,7 +1,5 @@
-"use strict";
-
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { SNSClient } from "@aws-sdk/client-sns";
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { SNSClient } from '@aws-sdk/client-sns';
 
 const client = new DynamoDBClient({ region: process.env.AWS_REGION });
 const snsClient = new SNSClient({ region: process.env.AWS_REGION });
@@ -9,6 +7,6 @@ const snsClient = new SNSClient({ region: process.env.AWS_REGION });
 module.exports.service = async (event: any, context: any, callback: any) => {
   callback(null, {
     statusCode: 200,
-    body: "hello push server!",
+    body: 'hello push server!',
   });
 };
