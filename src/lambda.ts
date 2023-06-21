9d39d631bc14553d826a2ce93ca4e1b7b4e1e28b
dragontaek-lee <dragontaek98@naver.com>
2023-06-21 17:24:51 +0900

    feat: token business logic, subscribe topic #5

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,47 +1,64 @@
 import { DynamoDBClient, PutItemCommand, GetItemCommand, UpdateItemCommand, DeleteItemCommand } from '@aws-sdk/client-dynamodb';
 import { SNSClient } from '@aws-sdk/client-sns';
-import tokenCommand from './modules/tokenCommands';
+import tokenCommands from './modules/tokenCommands';
+import snsCommands from './modules/snsCommands';
+import response from './constants/response';
+import status from './constants/status';
+import statusCode from './constants/statusCode';
+import { Actions, UserStatus, RequestDTO } from './types';
+import responseMessage from './constants/responseMessage';
 
 const client = new DynamoDBClient({ region: process.env.AWS_REGION });
 const snsClient = new SNSClient({ region: process.env.AWS_REGION });
 
-export const service = async (event: any, context: any, callback: any): Promise<any> => {
-  const data = JSON.parse(event.body);
+export const service = async (event: any): Promise<any> => {
+  const data: RequestDTO = JSON.parse(event.body);
 
   const { fcmToken, userId } = data;
 
-  let body;
-
   try {
     switch (event.headers.action) {
-      case 'register':
-        const getCommand = tokenCommand.getToken(fcmToken, userId);
-        const createCommand = tokenCommand.createToken(fcmToken, userId);
+      case Actions.REGISTER:
+        const getCommand = tokenCommands.getToken(fcmToken, userId);
+        const createCommand = tokenCommands.createToken(fcmToken, userId);
+        const subscribeCommand = snsCommands.subscribe(fcmToken);
 
         const data = await client.send(getCommand);
 
         if (!data.Item) {
+          if (userId != UserStatus.UNREGISTERED) {
+            const getUnRegisterCommand = tokenCommands.getToken(fcmToken, UserStatus.UNREGISTERED);
+            const unRegisteredData = await client.send(getUnRegisterCommand);
+
+            if (unRegisteredData.Item) {
+              const deleteUnRegisteredCommand = tokenCommands.deleteToken(fcmToken, UserStatus.UNREGISTERED);
+              await client.send(deleteUnRegisteredCommand);
+            }
+          }
+
+          await snsClient.send(subscribeCommand);
+
           await client.send(createCommand);
+        } else {
+          return response(400, status.fail(statusCode.BAD_REQUEST, responseMessage.DUPLICATED_TOKEN));
         }
 
-        body = 'register success';
+        return response(200, status.success(statusCode.OK, responseMessage.TOKEN_REGISTER_SUCCESS));
 
-        break;
-      case 'cancel':
-        const deleteCommand = tokenCommand.deleteToken(fcmToken, userId);
-        await client.send(deleteCommand);
+      case Actions.CANCEL:
+        const deleteCommand = tokenCommands.deleteToken(fcmToken, userId);
+        const result = await client.send(deleteCommand);
 
-        body = 'cancel success';
-        break;
+        if (!result.Attributes) {
+          return response(200, status.success(statusCode.OK, responseMessage.TOKEN_NOT_EXIST));
+        }
+
+        return response(200, status.success(statusCode.OK, responseMessage.TOKEN_CANCEL_SUCCESS));
       default:
         throw new Error(`Unsupported action: "${event.headers.action}"`);
     }
   } catch (e) {
-    callback(e);
-  } finally {
-    callback(null, {
-      statusCode: 200,
-      body: body,
-    });
+    console.error(e);
+    return response(500, responseMessage.INTERNAL_SERVER_ERROR);
   }
 };
