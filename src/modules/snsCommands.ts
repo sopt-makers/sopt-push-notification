171b4b64dc3f6f0d6f873102130913e1f9849515
dragontaek-lee <dragontaek98@naver.com>
2023-06-22 17:02:07 +0900

    feat: unsubscribe, cancel endpoint #5

--- src/modules/snsCommands.ts
+++ src/modules/snsCommands.ts
@@ -1,4 +1,9 @@
-import { SubscribeCommand } from '@aws-sdk/client-sns';
+import {
+  SubscribeCommand,
+  CreatePlatformEndpointCommand,
+  UnsubscribeCommand,
+  DeleteEndpointCommand,
+} from '@aws-sdk/client-sns';
 
 const subscribe = (arn: string) => {
   const command = new SubscribeCommand({
@@ -10,8 +15,39 @@ const subscribe = (arn: string) => {
   return command;
 };
 
+const unSubscribe = (arn: string) => {
+  const command = new UnsubscribeCommand({
+    SubscriptionArn: arn,
+  });
+
+  return command;
+};
+
+const registerEndPoint = (fcmToken: string, platform: string) => {
+  const platformApplicationArn =
+    platform == 'iOS' ? process.env.PLATFORM_APPLICATION_iOS : process.env.PLATFORM_APPLICATION_ANDROID;
+
+  const command = new CreatePlatformEndpointCommand({
+    PlatformApplicationArn: platformApplicationArn,
+    Token: fcmToken,
+  });
+
+  return command;
+};
+
+const cancelEndPoint = (arn: string) => {
+  const command = new DeleteEndpointCommand({
+    EndpointArn: arn,
+  });
+
+  return command;
+};
+
 const snsCommands = {
   subscribe,
+  unSubscribe,
+  registerEndPoint,
+  cancelEndPoint,
 };
 
 export default snsCommands;
