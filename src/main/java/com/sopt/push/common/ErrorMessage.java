c9cf6eaebba8426912f68b57bbd51e6352b13e72
seoyeonjiin <duswls214@khu.ac.kr>
2025-12-06 23:02:45 +0900

    [FIX] SEND_FAIL 에러 코드 500으로 변경

--- src/main/java/com/sopt/push/common/ErrorMessage.java
+++ src/main/java/com/sopt/push/common/ErrorMessage.java
@@ -5,9 +5,9 @@ public enum ErrorMessage {
   INVALID_REQUEST(StatusCode.BAD_REQUEST, "잘못된 요청입니다."),
   NULL_VALUE(StatusCode.BAD_REQUEST, "필요한 값이 없습니다."),
   TOKEN_NOT_EXIST(StatusCode.BAD_REQUEST, "존재하지 않는 토큰입니다."),
-  SEND_FAIL(StatusCode.BAD_REQUEST, "메시지 전송 실패."),
 
   /** 500 Internal Server Error */
+  SEND_FAIL(StatusCode.INTERNAL_SERVER_ERROR, "메시지 전송 실패."),
   INTERNAL_SERVER_ERROR(StatusCode.INTERNAL_SERVER_ERROR, "서버 내부 오류");
 
   private final int httpStatus;
