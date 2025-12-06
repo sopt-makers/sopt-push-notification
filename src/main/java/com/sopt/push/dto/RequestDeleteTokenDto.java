35a24868e488e09e590884f9cc549df19db6ed11
seoyeonjiin <duswls214@khu.ac.kr>
2025-12-06 22:14:22 +0900

    [FEAT] RequestDeleteTokenDto 추가

---
+++ src/main/java/com/sopt/push/dto/RequestDeleteTokenDto.java
@@ -0,0 +1,15 @@
+package com.sopt.push.dto;
+
+import com.sopt.push.enums.Platform;
+import com.sopt.push.enums.Services;
+
+import java.util.List;
+
+public record RequestDeleteTokenDto(
+        String transactionId,
+        Services service,
+        Platform platform,
+        String deviceToken,
+        List<String> userIds
+) {
+}
