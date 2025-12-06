7fbb01cc95b4b98857d0811b7b1881bf148592a3
seoyeonjiin <duswls214@khu.ac.kr>
2025-12-06 22:07:07 +0900

    [FEAT] Platform enum 추가

--- src/main/java/com/sopt/push/constants/Platform.java
+++
@@ -1,5 +0,0 @@
-package com.sopt.push.constants;
-
-public enum Platform {
-// TODO: Define platform enum
-}
---
+++ src/main/java/com/sopt/push/enums/Platform.java
@@ -0,0 +1,45 @@
+package com.sopt.push.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.sopt.push.common.BusinessException;
+import com.sopt.push.common.ErrorMessage;
+
+public enum Platform {
+    IOS("iOS", PushTopic.APNS),
+    ANDROID("Android", PushTopic.FCM);
+
+    private final String value;
+    private final PushTopic topic;
+
+    Platform(String value, PushTopic topic) {
+        this.value = value;
+        this.topic = topic;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return this.value;
+    }
+
+    public PushTopic getTopic() {
+        return topic;
+    }
+
+    @JsonCreator
+    public static Platform fromValue(String value) {
+        if (value == null) {
+            throw new BusinessException(
+                    ErrorMessage.INVALID_REQUEST, "Platform value cannot be null");
+        }
+
+        return switch (value) {
+            case "iOS" -> IOS;
+            case "Android" -> ANDROID;
+            default -> throw new BusinessException(
+                    ErrorMessage.INVALID_REQUEST,
+                    "Unknown Platform: " + value
+            );
+        };
+    }
+}
