2cee80750a710ab64a599ab352d246227a899e3e
seoyeonjiin <duswls214@khu.ac.kr>
2025-12-06 22:18:52 +0900

    [FEAT] FCM, APNS 메시지 빌더 추가

---
+++ src/main/java/com/sopt/push/message/AllMessageBuilder.java
@@ -0,0 +1,20 @@
+package com.sopt.push.message;
+
+import com.sopt.push.common.Constants;
+import com.sopt.push.dto.MessageFactoryDto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AllMessageBuilder {
+
+    private AllMessageBuilder() {}
+
+    public static Map<String, Object> build(MessageFactoryDto dto) {
+        Map<String, Object> message = new HashMap<>();
+        message.put("default", Constants.DEFAULT_MESSAGE);
+        message.put("APNS", ApnsMessageBuilder.build(dto).get("APNS"));
+        message.put("GCM", FcmMessageBuilder.build(dto).get("GCM"));
+        return message;
+    }
+}
---
+++ src/main/java/com/sopt/push/message/ApnsMessageBuilder.java
@@ -0,0 +1,38 @@
+package com.sopt.push.message;
+
+import com.sopt.push.common.Constants;
+import com.sopt.push.dto.MessageFactoryDto;
+import com.sopt.push.util.JsonUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+
+public class ApnsMessageBuilder {
+
+    private ApnsMessageBuilder() {}
+
+    public static Map<String, Object> build(MessageFactoryDto dto) {
+        Map<String, Object> aps = Map.of(
+                "alert", Map.of(
+                        "title", dto.title(),
+                        "body", dto.content()
+                )
+        );
+
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("aps", aps);
+        payload.put("category", dto.category().name());
+        payload.put("id", dto.id());
+        payload.put("webLink", dto.webLink());
+        payload.put("deepLink", dto.deepLink());
+        payload.put("sendAt", now());
+
+        Map<String, Object> wrapper = new HashMap<>();
+        wrapper.put("default", Constants.DEFAULT_MESSAGE);
+        wrapper.put("APNS", JsonUtil.toJson(payload));
+
+        return wrapper;
+    }
+}
