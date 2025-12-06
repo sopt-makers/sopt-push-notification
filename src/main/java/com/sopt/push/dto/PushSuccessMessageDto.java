461244b6a0e47f5df025ba2fc3163c52ed8afd15
seoyeonjiin <duswls214@khu.ac.kr>
2025-12-06 22:14:42 +0900

    [FEAT] PushSuccessMessageDto 추가

---
+++ src/main/java/com/sopt/push/dto/PushSuccessMessageDto.java
@@ -0,0 +1,20 @@
+package com.sopt.push.dto;
+
+import com.sopt.push.enums.Category;
+import com.sopt.push.enums.Services;
+import com.sopt.push.enums.WebHookType;
+
+import java.util.List;
+
+public record PushSuccessMessageDto(
+        String id,
+        String title,
+        String content,
+        Category category,
+        Services service,
+        WebHookType type,
+        String deepLink,
+        String webLink,
+        List<String> userIds
+) {
+}
