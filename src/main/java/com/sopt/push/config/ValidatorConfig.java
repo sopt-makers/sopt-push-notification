98dd64e23eda52e65033aae65e578de59abbbbe1
seoyeonjiin <duswls214@khu.ac.kr>
2025-12-12 17:18:32 +0900

    [FEAT] Validator 추가

--- src/main/java/com/sopt/push/config/ValidatorConfig.java
+++ src/main/java/com/sopt/push/config/ValidatorConfig.java
@@ -1,5 +1,27 @@
 package com.sopt.push.config;
 
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
+
 public class ValidatorConfig {
-  // TODO: Configure Bean Validation
+
+  private static final Validator VALIDATOR = createValidator();
+
+  private static Validator createValidator() {
+    ValidatorFactory factory =
+        Validation.byDefaultProvider()
+            .configure()
+            .messageInterpolator(new ParameterMessageInterpolator())
+            .buildValidatorFactory();
+
+    return factory.getValidator();
+  }
+
+  public static Validator getValidator() {
+    return VALIDATOR;
+  }
+
+  private ValidatorConfig() {}
 }
---
+++ src/main/java/com/sopt/push/util/ValidationUtil.java
@@ -0,0 +1,22 @@
+package com.sopt.push.util;
+
+import com.sopt.push.common.BusinessException;
+import com.sopt.push.common.ErrorMessage;
+import com.sopt.push.config.ValidatorConfig;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import java.util.Set;
+
+public final class ValidationUtil {
+
+  private static final Validator VALIDATOR = ValidatorConfig.getValidator();
+
+  private ValidationUtil() {}
+
+  public static <T> void validate(T dto) {
+    Set<ConstraintViolation<T>> violations = VALIDATOR.validate(dto);
+    if (!violations.isEmpty()) {
+      throw new BusinessException(ErrorMessage.INVALID_REQUEST);
+    }
+  }
+}
