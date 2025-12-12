3f7f844559fb9f79384690b0cf9b7af3e976beb7
seoyeonjiin <duswls214@khu.ac.kr>
2025-12-12 21:41:13 +0900

    [FEAT] client region, credential 설정 제거

--- src/main/java/com/sopt/push/client/DynamoDbClientProvider.java
+++ src/main/java/com/sopt/push/client/DynamoDbClientProvider.java
@@ -1,8 +1,6 @@
 package com.sopt.push.client;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 public final class DynamoDbClientProvider {
@@ -10,12 +8,7 @@ public final class DynamoDbClientProvider {
   private static final DynamoDbEnhancedClient ENHANCED_CLIENT;
 
   static {
-    DynamoDbClient standardClient =
-        DynamoDbClient.builder()
-            .credentialsProvider(DefaultCredentialsProvider.create())
-            .region(Region.AP_NORTHEAST_2)
-            .build();
-
+    DynamoDbClient standardClient = DynamoDbClient.builder().build();
     ENHANCED_CLIENT = DynamoDbEnhancedClient.builder().dynamoDbClient(standardClient).build();
   }
 
--- src/main/java/com/sopt/push/client/SnsClientProvider.java
+++ src/main/java/com/sopt/push/client/SnsClientProvider.java
@@ -1,7 +1,5 @@
 package com.sopt.push.client;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 public class SnsClientProvider {
@@ -9,11 +7,7 @@ public class SnsClientProvider {
   private static final SnsClient snsClient;
 
   static {
-    snsClient =
-        SnsClient.builder()
-            .region(Region.AP_NORTHEAST_2)
-            .credentialsProvider(DefaultCredentialsProvider.create())
-            .build();
+    snsClient = SnsClient.builder().build();
   }
 
   private SnsClientProvider() {}
