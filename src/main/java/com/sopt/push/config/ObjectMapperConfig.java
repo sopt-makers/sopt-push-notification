28d73f3e348c7e42742bbefe32fc5c47e7ca42e4
seoyeonjiin <duswls214@khu.ac.kr>
2025-12-12 17:18:56 +0900

    [FEAT] ObjectMapperConfig 추가

--- src/main/java/com/sopt/push/config/ObjectMapperConfig.java
+++ src/main/java/com/sopt/push/config/ObjectMapperConfig.java
@@ -1,21 +1,24 @@
 package com.sopt.push.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperConfig {
 
-  private static final ObjectMapper objectMapper = new ObjectMapper();
+  private static final ObjectMapper MAPPER = createObjectMapper();
 
-  private ObjectMapperConfig() {}
-
-  static {
-    objectMapper.registerModule(new JavaTimeModule());
-    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+  private static ObjectMapper createObjectMapper() {
+    return new ObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        .registerModule(new JavaTimeModule());
   }
 
   public static ObjectMapper getObjectMapper() {
-    return objectMapper;
+    return MAPPER;
   }
+
+  private ObjectMapperConfig() {}
 }
