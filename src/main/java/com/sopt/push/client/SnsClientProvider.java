73de40e4d46525f5e99e8c3b5c34e99591c51825
seoyeonjiin <duswls214@khu.ac.kr>
2025-12-12 17:15:08 +0900

    [FEAT] Sns client 설정

--- src/main/java/com/sopt/push/client/SnsClientProvider.java
+++ src/main/java/com/sopt/push/client/SnsClientProvider.java
@@ -1,5 +1,24 @@
 package com.sopt.push.client;
 
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+
 public class SnsClientProvider {
-  // TODO: Provide SNS client singleton
+
+  private static final SnsClient snsClient;
+
+  static {
+    snsClient =
+        SnsClient.builder()
+            .region(Region.AP_NORTHEAST_2)
+            .credentialsProvider(DefaultCredentialsProvider.create())
+            .build();
+  }
+
+  private SnsClientProvider() {}
+
+  public static SnsClient getClient() {
+    return snsClient;
+  }
 }
