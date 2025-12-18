bf5053c41e363fec6c5dab73e59c4d47e2709c81
sung-silver <jinna0319@gmail.com>
2025-12-18 23:07:01 +0900

    [FEAT] UserService에 토큰 무효화 로직 구현
    - SNS 구독 해제
    - SNS 엔드포인트 삭제
    - DynamoDB 토큰 레코드 삭제

--- src/main/java/com/sopt/push/service/UserService.java
+++ src/main/java/com/sopt/push/service/UserService.java
@@ -1,5 +1,23 @@
 package com.sopt.push.service;
 
+import com.sopt.push.domain.DeviceTokenEntity;
+import com.sopt.push.repository.DeviceTokenRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class UserService {
-  // TODO: Implement user service
+
+  private final DeviceTokenRepository deviceTokenRepository;
+  private final SnsService snsService;
+
+  public void unregisterToken(DeviceTokenEntity deviceTokenEntity) {
+    String subscriptionArn = deviceTokenEntity.getSubscriptionArn();
+    String endpointArn = deviceTokenEntity.getEndpointArn();
+    String pk = deviceTokenEntity.getPk();
+    String sk = deviceTokenEntity.getSk();
+
+    snsService.unsubscribe(subscriptionArn);
+    snsService.cancelEndpoint(endpointArn);
+    deviceTokenRepository.delete(pk, sk);
+  }
 }
