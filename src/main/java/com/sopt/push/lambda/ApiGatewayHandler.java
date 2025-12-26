a46e6383705d93177786193c6e55680bb9af806b
정윤 <sju08227@naver.com>
2025-12-26 21:39:52 +0900

    [REFACTOR] spotleess 적용

--- src/main/java/com/sopt/push/lambda/ApiGatewayHandler.java
+++ src/main/java/com/sopt/push/lambda/ApiGatewayHandler.java
@@ -38,7 +38,6 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
