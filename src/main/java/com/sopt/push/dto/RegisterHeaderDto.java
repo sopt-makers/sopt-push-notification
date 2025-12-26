4593796b7b49c7db6d027971e0ef7d18cc85297d
정윤 <sju08227@naver.com>
2025-12-26 21:31:33 +0900

    [REFACTOR] 불필요한 코드 제거

--- src/main/java/com/sopt/push/dto/RegisterHeaderDto.java
+++ src/main/java/com/sopt/push/dto/RegisterHeaderDto.java
@@ -5,11 +5,4 @@ import com.sopt.push.enums.Platform;
 import com.sopt.push.enums.Services;
 
 public record RegisterHeaderDto(
-    String transactionId, Services service, Platform platform, Actions action) {
-
-  public RegisterHeaderDto {
-    if ((action == Actions.REGISTER || action == Actions.CANCEL) && platform == null) {
-      throw new IllegalArgumentException("Platform is required for REGISTER and CANCEL actions");
-    }
-  }
-}
+    String transactionId, Services service, Platform platform, Actions action) {}
