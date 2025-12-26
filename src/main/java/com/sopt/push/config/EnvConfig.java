09cee502021b57b111078b5705ce9cc3710b8f45
정윤 <sju08227@naver.com>
2025-12-26 21:30:39 +0900

    [FEAT] EnvConfig env 추가
    - 토큰 등록시 aws 엔드포인트 등록을 위한 env를 추가합니다.

--- src/main/java/com/sopt/push/config/EnvConfig.java
+++ src/main/java/com/sopt/push/config/EnvConfig.java
@@ -9,17 +9,23 @@ public final class EnvConfig {
   private static final String ALL_TOPIC_ARN_ENV_VAR = "ALL_TOPIC_ARN";
   private static final String MAKERS_APP_SERVER_URL = "MAKERS_APP_SERVER_URL";
   private static final String MAKERS_OPERATION_SERVER_URL = "MAKERS_OPERATION_SERVER_URL";
+  private static final String PLATFORM_APPLICATION_IOS_ENV = "PLATFORM_APPLICATION_iOS";
+  private static final String PLATFORM_APPLICATION_ANDROID_ENV = "PLATFORM_APPLICATION_ANDROID";
 
   private final String dynamoDbTableName;
   private final String allTopicArn;
   private final String makersAppServerUrl;
   private final String makersOperationServerUrl;
+  private final String platformApplicationIosArn;
+  private final String platformApplicationAndroidArn;
 
   public EnvConfig() {
     this.dynamoDbTableName = getRequiredEnv(DYNAMODB_TABLE_ENV_VAR);
     this.allTopicArn = getRequiredEnv(ALL_TOPIC_ARN_ENV_VAR);
     this.makersAppServerUrl = getRequiredEnv(MAKERS_APP_SERVER_URL);
     this.makersOperationServerUrl = getRequiredEnv(MAKERS_OPERATION_SERVER_URL);
+    this.platformApplicationIosArn = getRequiredEnv(PLATFORM_APPLICATION_IOS_ENV);
+    this.platformApplicationAndroidArn = getRequiredEnv(PLATFORM_APPLICATION_ANDROID_ENV);
   }
 
   private static String getRequiredEnv(String key) {
